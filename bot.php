32a621b3f2ac8739b6f222fba91a0acea2a6350d
kraofthai <46700371+kraofthai@users.noreply.github.com>
2019-03-30 18:54:42 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,7 +1,7 @@
 <?php
 $access_token = 'rheP69VO/QegnihKdbJ5zdmwNxutb3gbCQ9Mi2AbYRjwzjjp82X4BhS7npIPXYhrPd9SZ4ssq6CzJHm1+CiLNEaD1TGTUR+cXYhmorpmO1bpr095yLnduNosdIOqLw7cgOTQaSKOgwPjTgqlPp1o/gdB04t89/1O/w1cDnyilFU=
 ';
-$url = 'https://api.line.me/v1/oauth/verify';
+$url = 'https://api.line.me/oauth2/v2.1/verify';
 $headers = array('Authorization: Bearer ' . $access_token);
 $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
