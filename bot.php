043350c92d0191f8970d7d858b1ce4ac1b86b994
kraofthai <46700371+kraofthai@users.noreply.github.com>
2019-03-30 18:46:15 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -1,4 +1,13 @@
-<?php 
-echo "Hello World";
+<?php
+$access_token = 'pk42Vq1oXfJcRmu5oIfXDaUQtXWGlbaHFzIZwhpOtS2iOtdWqHjkF49xMYkTMD28Pd9SZ4ssq6CzJHm1+CiLNEaD1TGTUR+cXYhmorpmO1akrJzvT3HpLwamTNtA97bI05NWDPCvoiMjCXWmVBPvSAdB04t89/1O/w1cDnyilFU=
+';
+$url = 'https://api.line.me/v1/oauth/verify';
+$headers = array('Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);curl_close($ch);
+echo $result;
 
 ?>
